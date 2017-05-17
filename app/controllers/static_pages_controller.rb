cd34954d536b2f6085bb360d68446cce5c62a3f2
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-05-17 13:42:18 +0000

    Added some random shitty things

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
+
   end
 
   def help
   end
+
+  def flipcoin
+    @coin = rand.round
+    if @coin == 1
+      result = "heads"
+    else 
+      result = "tails"
+    end
+  end
+
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def flipcoin
+    @coin = rand.round
+    if @coin == 1
+      result = "heads"
+    else 
+      result = "tails"
+    end
+  end
 end
