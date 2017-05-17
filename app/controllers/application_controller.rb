be847663bdb2c69c1155502e8153e43f3795c752
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-05-17 12:02:13 +0000

    Added index page with random Chuck Norris joke

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "<%= Faker::ChuckNorris.fact %>"
+    render 'static_pages/home'
   end
 end
