c5c8812c472fce19305403474aca4d1128c47194
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-05-19 08:55:07 +0000

    added CSS and additional counters

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -8,6 +8,7 @@ module StaticPagesHelper
       cookies[:lastresult] = 0
       cookies[:consmatches] = 0
       cookies[:previousresult] = 0
+      cookies[:mostconsmatches] = 0
       @msg = "Empty cookie, setting all values to 0"
     else
       @msg = "Cookie exists"
@@ -26,10 +27,18 @@ module StaticPagesHelper
     cookies[:lastresult] = @result
   end
   
-  def consecutives
+  def consecutives # keep track of consecutive matches
     if cookies[:lastresult] == cookies[:previousresult]
-      # increase consecutive matches
-      cookies[:consmatches] = cookies[:consmatches].to_i + 1 
+      # increase the cookie-based counter, first series go from 0 to 2
+      if cookies[:consmatches].to_i == 0
+        cookies[:consmatches] = 2
+      else
+        cookies[:consmatches] = cookies[:consmatches].to_i + 1
+      end
+      # compare to the biggest recorded match, store if bigger
+      if cookies[:consmatches].to_i > cookies[:mostconsmatches].to_i
+        cookies[:mostconsmatches] = cookies[:consmatches]
+      end
     else
       # reset counter
       cookies[:consmatches] = 0
