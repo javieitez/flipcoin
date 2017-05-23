34addb66aaa0089ed18254e790c71b307f1d0e7b
JA Vieitez <javieitez@theusualfreemailprovider.com>
2017-05-23 12:26:56 +0000

    Total refactor, several bugfixes

---
+++ app/helpers/initcookies.rb
@@ -0,0 +1,21 @@
+# Init all cookie values to defaults
+def checkforcookie
+  if cookies[:created_at].nil?
+    cookies[:totalheads] = 0
+    cookies[:totaltails] = 0
+    cookies[:currentresult] = 0
+    cookies[:consheads] = 1
+    cookies[:constails] = 1
+    cookies[:tailsrecord] = 0
+    cookies[:headsrecord] = 0
+    cookies[:previousresult] = 0
+    cookies[:totalflips] = 1
+    cookies[:pingpongs] = 0
+    cookies[:pingpongsrecord] = 0
+    cookies[:created_at] = Time.now
+    @msg = "Empty cookie, starting from scratch"
+  else
+    @msg = "Started #{time_ago_in_words(cookies[:created_at])} ago."
+  end
+end
+
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,83 +1,52 @@
 module StaticPagesHelper
 
-  # Init all cookie values to defaults
-  def checkforcookie
-    if cookies[:created_at].nil?
-      cookies[:headseries] = 0
-      cookies[:tailseries] = 0
-      cookies[:lastresult] = 0
-      cookies[:consheads] = 0
-      cookies[:constails] = 0
-      cookies[:tailsrecord] = 0
-      cookies[:headsrecord] = 0
-      cookies[:consmatches] = 0
-      cookies[:previousresult] = 0
-      cookies[:totalflips] = 0
-      cookies[:pingpongs] = 0
-      cookies[:pingpongsrecord] = 0
-      cookies[:created_at] = Time.now
-      @msg = "Empty cookie, setting all values to 0"
-    else
-      @msg = "Started #{time_ago_in_words(cookies[:created_at])} ago."
-    end
-  end
+require 'initcookies'
 
-  def flipcoin
-    @coin = rand.round
-    cookies[:totalflips] = cookies[:totalflips].to_i + 1
-    if @coin == 1
-      cookies[:headseries] = cookies[:headseries].to_i + 1
-      @result = 'heads'
-    else 
-      cookies[:tailseries] = cookies[:tailseries].to_i + 1
-      @result = 'tails'
-    end
-    cookies[:lastresult] = @result
-  end
-  
-  def consecutives # keep track of consecutive matches
-    if cookies[:lastresult] == cookies[:previousresult] and cookies[:lastresult] == 'heads'
-      compareheads
-      increase_counter
-      compare_matches
-    elsif cookies[:lastresult] == cookies[:previousresult] and cookies[:lastresult] == 'tails'
-      comparetails
-      increase_counter
-      compare_matches
-    else
-      reset_counter
-      compare_pingpongs
-    end
-    @result = cookies[:consmatches] 
-  end
-
-def increase_counter
-  # reset the pingpongs counter
-    cookies[:pingpongs] = 0
-  # increase the matches counter, first series go from 0 to 2
-  if cookies[:consmatches].to_i == 0
-    cookies[:consmatches] = 2
+def flipcoin
+  @coin = rand.round
+  cookies[:totalflips] = cookies[:totalflips].to_i + 1
+  if @coin == 1
+    cookies[:totalheads] = cookies[:totalheads].to_i + 1
+    @result = 'heads'
   else
-    cookies[:consmatches] = cookies[:consmatches].to_i + 1
+    cookies[:totaltails] = cookies[:totaltails].to_i + 1
+    @result = 'tails'
+  end
+  cookies[:currentresult] = @result
+end
+
+def consecutives # keep track of consecutive matches
+  @current = cookies[:currentresult]
+  @prev = cookies[:previousresult]
+  if @current ==  @prev and @current == 'heads'
+    compareheads
+    cookies[:consheads] = cookies[:consheads].to_i + 1
+    check_new_high
+    cookies[:pingpongs] = 0
+  elsif @current == @prev and @current == 'tails'
+    comparetails
+    cookies[:constails] = cookies[:constails].to_i + 1
+    check_new_high
+    cookies[:pingpongs] = 0
+  else
+    compare_pingpongsrecord
+    cookies[:previousresult] = cookies[:currentresult]
+    cookies[:pingpongs] = cookies[:pingpongs].to_i + 1
+    cookies[:consheads] = 0
+    cookies[:constails] = 0
   end
 end
 
-def compare_matches
+def check_new_high
   if newrecord?
     flash[:notice] = "New Record on on consecutive flips"
   end
 end
 
-def compare_pingpongs
-  if newpingpongrecord?
-    flash[:notice] = "New Record on alternate flips"
-    cookies[:pingpongsrecord] = cookies[:pingpongs]
-  end
-end
-
 def newpingpongrecord?
-  if cookies[:pingpongs].to_i > cookies[:pingpongsrecord].to_i \
-                              and cookies[:pingpongs].to_i > 1 
+  @amount = cookies[:pingpongs].to_i
+  @record = cookies[:pingpongsrecord].to_i
+  if @amount  > @record and @amount > 1
     true
   else
     false
@@ -86,50 +55,38 @@ end
 
 
 def newrecord?
-  if cookies[:consmatches].to_i > 0 and cookies[:consmatches].to_i > \
-    [cookies[:tailsrecord].to_i, cookies[:headsrecord].to_i].max
+  @cons = [cookies[:constails].to_i, cookies[:consheads].to_i].max
+  @rec = [cookies[:tailsrecord].to_i, cookies[:headsrecord].to_i].max
+  if @rec > 0 and @cons > @rec
     true
   else
     false
   end
 end
 
+def compareheads
+  @cons = cookies[:consheads].to_i
+  @rec = cookies[:headsrecord].to_i
+  cookies[:constails] = 0 # reset tails series
+  if cookies[:consheads].to_i > @rec #new record?
+    cookies[:headsrecord] = cookies[:consheads] # write the cookie, not the var
+  end
+end
 
 def comparetails
-  if cookies[:constails].to_i > cookies[:tailsrecord].to_i
-    cookies[:tailsrecord] = cookies[:constails] 
-  end
-  # first series go from 0 to 2
-  if cookies[:constails].to_i == 0
-    cookies[:constails] = 2
-  else
-    cookies[:constails] = cookies[:constails].to_i + 1  
-    cookies[:consheads] = 0
+  @cons = cookies[:constails].to_i
+  @rec = cookies[:tailsrecord].to_i
+  cookies[:consheads] = 0 # reset tails series
+  if cookies[:constails].to_i > @rec #new record?
+    cookies[:tailsrecord] = cookies[:constails] # write the cookie, not the var
   end
 end
 
-def compareheads
-  if cookies[:consheads].to_i > cookies[:headsrecord].to_i
-    cookies[:headsrecord] = cookies[:consheads] 
-  end
-  # first series go from 0 to 2
-  if cookies[:consheads].to_i == 0
-    cookies[:consheads] = 2
-  else
-  cookies[:consheads] = cookies[:consheads].to_i + 1  
-  cookies[:constails] = 0
+def compare_pingpongsrecord
+  if newpingpongrecord?
+    flash[:notice] = "New Record on alternate flips"
+    cookies[:pingpongsrecord] = cookies[:pingpongs]
   end
 end
 
-def reset_counter
-    cookies[:consmatches] = 0
-    cookies[:previousresult] = cookies[:lastresult]
-    cookies[:pingpongs] = cookies[:pingpongs].to_i + 1
 end
-
-def pingpongscounter
-  cookies[:pingpongs] = cookies[:pingpongs].to_i + 1
-end
-
-end
-
